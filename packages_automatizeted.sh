bcc5144aa0d76ebc16db2b01d880a3c9ef64e200
Rita de Cassia <ritadecassia.infor2@gmail.com>
2023-07-09 18:33:58 -0300

    feat: Add commands to install intellij

---
+++ packages_automatizeted.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "The intelliji will be installed"
+
+sudo pacman -Syu
+
+# packages=("grep", "curl", "wget", "tar", "jdk-openjdk")
+packages=("grep", "curl", "wget")
+
+for package in "${packages[@]}"; do
+    if package -Q "$package" > /dev/null 2>&1; then
+        echo "$package already exists."
+    else
+        sudo pacman -S "$package"
+    fi
+done
+
+# Get the intelliji current version from an api
+response=$(curl -sL "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release")
+downloadUrl=$(echo "$response" | grep -oP '"linux":{"link":"\K[^"]+')
+filename=$(echo "$downloadUrl" | grep -oP '/[^/]+\.tar.gz$' | tail -n1)
+path="$HOME/Documentos/packages_test"
+
+cd $path
+
+echo $filename
+
+# wget "$downloadUrl"
+
+# tar -xvfz 
