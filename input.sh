cd8d817ebd23c7ed1cc38beb96a184b935490efb
Rita de Cassia <ritadecassia.infor2@gmail.com>
2023-08-04 18:08:46 -0300

    fix: fix input variable

--- input.sh
+++ input.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "How is your linux destribuition?"
-read destribuition
+echo "How package manager do you want use? Ex: apt, pacman"
+read package_manager
 
-if [ "$destribuition" = "arch" ]; then
-    echo "Alright"
+if [ "$package_manager" = "pacman" ]; then
+    sudo pacman -Syu
 else
-    echo "We dont have support to this distribuition"
+    echo "We dont have support to this package manager"
 fi
