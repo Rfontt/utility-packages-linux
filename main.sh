34d7b2fd8a5cb2bd198862118a0bb3616e65d2a0
Rita de Cassia <ritadecassia.infor2@gmail.com>
2023-09-30 16:59:31 -0300

    feat: Add menu

--- main.sh
+++ main.sh
@@ -5,17 +5,19 @@ temp_dir=$(mktemp -d -p "$HOME" packages.XXXXXX)
 . ./packagesProvider.sh
 . ./transformToApp.sh
 . ./extractDownloadFolder.sh
+. ./menu.sh
 
 packageManager
 installPackages
+showMenu
 
-response=$(curl -sL "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release")
-downloadUrl=$(echo "$response" | grep -oP '"linux":{"link":"\K[^"]+')
+# response=$(curl -sL "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release")
+# downloadUrl=$(echo "$response" | grep -oP '"linux":{"link":"\K[^"]+')
 
-extractDownloadFolder "$downloadUrl" "$temp_dir"
+# extractDownloadFolder "$downloadUrl" "$temp_dir"
 
-intellij_dir="/opt/intellij-idea"
+# intellij_dir="/opt/intellij-idea"
 
-transformToApp "$intellij_dir" "$intellij_dir/bin/idea.png" "$intellij_dir/bin/idea.sh" "idea-IU" "/usr/local/bin/idea" "intellij-idea" "Powerful IDE for Java development" "Development;IDE;" "jetbrains-idea"
+# transformToApp "$intellij_dir" "$intellij_dir/bin/idea.png" "$intellij_dir/bin/idea.sh" "idea-IU" "/usr/local/bin/idea" "intellij-idea" "Powerful IDE for Java development" "Development;IDE;" "jetbrains-idea"
 
-rm -rf "$temp_dir"
+# rm -rf "$temp_dir"
